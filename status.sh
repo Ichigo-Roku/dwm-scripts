ff5ce380f2745d32f6a34f4a4b952552494ebf77
Ichigo-Roku <ichigo-roku@outlook.com>
2015-08-20 19:10:03 +0200

    Everything is more explicit of what is displayed.

--- status.sh
+++ status.sh
@@ -1,27 +1,27 @@
 _bat() {
     bat=`cat /sys/class/power_supply/BAT0/capacity`
-    bat=`echo -e "\x06[$bat%]"`
+    bat=`echo -e "\x06battery at $bat%"`
 }
 
 _volume() {
     status=$(amixer get Master | grep "Mono: P" | awk '{print $6}')
     if [ "$status" = "[on]" ] ; then
 	volume=$(amixer get Master | grep "Mono: P" | awk '{print $4}' | grep -oE "[[:digit:]]{1,}"%)
-	volume=`echo -e "\x05[$volume]"`
+	volume=`echo -e "\x05volume at $volume"`
     else
-	volume="mute"
-	volume=`echo -e "\x05[$volume]"`
+	volume="muted"
+	volume=`echo -e "\x05volume $volume"`
     fi
 }
 
 _heure() {
     heure=$(date '+%I:%M %p')
-    heure=`echo -e "\x01[$heure]"`
+    heure=`echo -e "\x01$heure"`
 }
 
 _date() {
     date=$(date '+%A, %B %d')
-    date=`echo -e "\x06[$date]"`
+    date=`echo -e "\x06$date"`
 }
 
 _uptime() {
@@ -36,27 +36,27 @@ _uptime() {
     if [ "${days}" -ne "0" ]; then
 	uptime="${days}d ${uptime}"
     fi
-    uptime=`echo -e "\x05[$uptime]"`
+    uptime=`echo -e "\x05uptime of $uptime"`
 }
 
 _kernel() {
     kernel=`uname -r`
-    kernel=`echo -e "\x01[$kernel]"`
+    kernel=`echo -e "\x01kernel $kernel"`
 }
 
 _moc() {
     if [ -z $(mocp -Q %state | grep PLAY) ]; then
 	mus="off"
-	moc=`echo -e "\x06[$mus]"`
+	moc=`echo -e "\x06music $mus"`
     else
 	art=$(mocp -Q %artist)
 	tit=$(mocp -Q %song)
 	if [ $(echo "$art - $tit" | wc -m) -gt "99" ]; then
 	    mus=$(echo "$art - $tit"  | cut -b 1-99)
-	    moc=`echo -e "\x06[$mus...]"`
+	    moc=`echo -e "\x06listening to $mus..."`
 	else
 	    mus="$art - $tit"
-	    moc=`echo -e "\x06[$mus]"`
+	    moc=`echo -e "\x06listening to $mus"`
 	fi
     fi
 }
@@ -65,14 +65,14 @@ _bright() {
     max=`cat /sys/class/backlight/intel_backlight/max_brightness`
     actual=`cat /sys/class/backlight/intel_backlight/actual_brightness`
     bright=`echo $(( ($actual * 100) / $max ))`
-    bright=`echo -e "\x05[$bright%]"`
+    bright=`echo -e "\x05screen brightness at $bright%"`
 }
 
 _light() {
     max=`cat /sys/class/leds/smc::kbd_backlight/max_brightness`
     actual=`cat /sys/class/leds/smc::kbd_backlight/brightness`
     light=`echo $(( ($actual * 100) / $max ))`
-    light=`echo -e "\x01[$light%]"`
+    light=`echo -e "\x01keyboard brightness at $light%"`
 }
 
 status() {
