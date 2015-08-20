c42c0a8f59d175d910f2193f66a025d2d12a3298
Ichigo-Roku <ichigo-roku@outlook.com>
2015-08-20 18:33:34 +0200

    Battery percentage added and color updated.

--- status.sh
+++ status.sh
@@ -1,22 +1,27 @@
+_bat() {
+    bat=`cat /sys/class/power_supply/BAT0/capacity`
+    bat=`echo -e "\x06[$bat%]"`
+}
+
 _volume() {
     status=$(amixer get Master | grep "Mono: P" | awk '{print $6}')
     if [ "$status" = "[on]" ] ; then
 	volume=$(amixer get Master | grep "Mono: P" | awk '{print $4}' | grep -oE "[[:digit:]]{1,}"%)
-	volume=`echo -e "\x06[$volume]"`
+	volume=`echo -e "\x05[$volume]"`
     else
 	volume="mute"
-	volume=`echo -e "\x06[$volume]"`
+	volume=`echo -e "\x05[$volume]"`
     fi
 }
 
 _heure() {
     heure=$(date '+%I:%M %p')
-    heure=`echo -e "\x05[$heure]"`
+    heure=`echo -e "\x01[$heure]"`
 }
 
 _date() {
     date=$(date '+%A, %B %d')
-    date=`echo -e "\x01[$date]"`
+    date=`echo -e "\x06[$date]"`
 }
 
 _uptime() {
@@ -31,27 +36,27 @@ _uptime() {
     if [ "${days}" -ne "0" ]; then
 	uptime="${days}d ${uptime}"
     fi
-    uptime=`echo -e "\x06[$uptime]"`
+    uptime=`echo -e "\x05[$uptime]"`
 }
 
 _kernel() {
     kernel=`uname -r`
-    kernel=`echo -e "\x05[$kernel]"`
+    kernel=`echo -e "\x01[$kernel]"`
 }
 
 _moc() {
     if [ -z $(mocp -Q %state | grep PLAY) ]; then
 	mus="off"
-	moc=`echo -e "\x01[$mus]"`
+	moc=`echo -e "\x06[$mus]"`
     else
 	art=$(mocp -Q %artist)
 	tit=$(mocp -Q %song)
 	if [ $(echo "$art - $tit" | wc -m) -gt "99" ]; then
 	    mus=$(echo "$art - $tit"  | cut -b 1-99)
-	    moc=`echo -e "\x01[$mus...]"`
+	    moc=`echo -e "\x06[$mus...]"`
 	else
 	    mus="$art - $tit"
-	    moc=`echo -e "\x01[$mus]"`
+	    moc=`echo -e "\x06[$mus]"`
 	fi
     fi
 }
@@ -64,4 +69,4 @@ status() {
     echo "$args"
 }
 
-status moc kernel uptime date heure volume
+status moc kernel uptime date heure volume bat
