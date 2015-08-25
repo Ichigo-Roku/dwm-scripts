c00b00a854dbafbdc86b775a0e7f7f601f6f4bf1
Ichigo-Roku <ichigo-roku@outlook.com>
2015-08-25 21:00:16 +0200

    Double m for minutes removed, only one m instead.

--- status.sh
+++ status.sh
@@ -55,7 +55,7 @@ _uptime() {
     days=$((${uptime}/86400))
     uptime="${mins}m"
     if [ "${hours}" -ne "0" ]; then
-	uptime="${hours}h${uptime}m"
+	uptime="${hours}h${uptime}"
     fi
     if [ "${days}" -ne "0" ]; then
 	uptime="${days}d${uptime}"
